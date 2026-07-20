5470bea3253b81fabfb963464c157a514339d717
venkatramreddi01 <pallevenkatramreddi@gmail.com>
2026-07-20 19:17:00 +0530

    Day 2: Valid Palindrome - Two Pointer Pattern

---
+++ src/twopointers/ValidPalindrome.java
@@ -0,0 +1,37 @@
+package twopointers;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(isPalindrome("race a car"));                     // false
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
